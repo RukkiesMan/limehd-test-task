db0eaca8f3418fa87f1932a0732729fdc98a53a8
Pavel Alekseev <pavelekseev@gmail.com>
2019-11-29 17:06:58 +0300

    Add swapi service to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,17 @@ import React from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { SwapiServiceProvider } from '../contexts/swapi-context';
+import SwapiService from '../api/services/swapi-service';
+
+const swapiService = new SwapiService();
+
 const App = () => {
-  return <div></div>;
+  return (
+    <SwapiServiceProvider value={swapiService}>
+      <div></div>
+    </SwapiServiceProvider>
+  );
 };
 
 export default App;
