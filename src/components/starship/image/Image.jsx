15783f3ff831adbddc754efe42b004d421a90000
Pavel Alekseev <pavelekseev@gmail.com>
2019-11-29 23:35:11 +0300

    Add large size to starship image

--- src/components/starship/image/Image.jsx
+++ src/components/starship/image/Image.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import Image from 'react-image';
 
@@ -8,23 +9,26 @@ import Spinner from '../../spinner';
 
 import './Image.scss';
 
-const StarshipImage = ({ imageService, starshipId, ...props }) => {
+const StarshipImage = ({ imageService, starshipId, size }) => {
   const imageURL = imageService.getStarshipImage(starshipId);
   const defaultImageURL = imageService.getDefaultStarshipImage();
+
+  const containerClassName = classNames({
+    'starship-image__container': true,
+    'starship-image__container_lg': size === 'lg',
+    'd-flex': true,
+    'justify-content-center': true,
+    'align-items-center': true,
+  });
 
   return (
     <Image
       className="starship-image"
       src={[imageURL, defaultImageURL]}
       container={children => {
-        return (
-          <div className="starship-image__container d-flex justify-content-center align-items-center">
-            {children}
-          </div>
-        );
+        return <div className={containerClassName}>{children}</div>;
       }}
       loader={<Spinner />}
-      {...props}
     />
   );
 };
