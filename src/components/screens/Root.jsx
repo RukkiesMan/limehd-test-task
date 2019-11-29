d937cebb953ac3c5109a1ae85156535ed6f6addf
Pavel Alekseev <pavelekseev@gmail.com>
2019-11-29 18:42:00 +0300

    Add path to display starship info screen

--- src/components/screens/Root.jsx
+++ src/components/screens/Root.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import ScreenHome from './home';
+import ScreenStarshipInfo from './starship/info';
 
 const ScreenRoot = () => {
   return (
     <Router>
       <Switch>
         <Route path="/" component={ScreenHome} exact />
+        <Route path="/starships/:id" component={ScreenStarshipInfo} />
       </Switch>
     </Router>
   );
