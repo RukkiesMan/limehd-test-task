23a4bc7c76f550a16a7893a245403793f4911780
Pavel Alekseev <pavelekseev@gmail.com>
2019-11-29 21:51:57 +0300

    Add starship image component to starship item

--- src/components/starship/item/Item.jsx
+++ src/components/starship/item/Item.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 import { Card } from 'react-bootstrap';
 
+import StarshipImage from '../image';
+
 const StarshipItem = ({ starship, ...props }) => {
   return (
     <Card {...props}>
+      <StarshipImage starshipId={starship.id} />
       <Card.Body>
         <Card.Title>{starship.name}</Card.Title>
       </Card.Body>
