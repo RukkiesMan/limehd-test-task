b7639f5e3aefc77dfcffb2d8887682035465457e
Pavel Alekseev <pavelekseev@gmail.com>
2019-11-30 11:11:59 +0300

    Add back button to info starship page

--- src/components/screens/starship/info/Info.jsx
+++ src/components/screens/starship/info/Info.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+
+import { Row, Col, Button } from 'react-bootstrap';
 
 import ScreenLayoutMain from '../../layouts/main';
 import StarshipInfo from '../../../starship/info';
 
 const ScreenStarshipInfo = ({ match }) => {
+  const history = useHistory();
+
   return (
     <ScreenLayoutMain>
+      <Row className="mb-3">
+        <Col>
+          <Button variant="secondary" onClick={() => history.push('/')}>
+            Back
+          </Button>
+        </Col>
+      </Row>
       <StarshipInfo starshipId={match.params.id} />
     </ScreenLayoutMain>
   );
